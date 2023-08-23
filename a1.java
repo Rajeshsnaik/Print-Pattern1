a604d7fbc7c542560e3055a74fbb75b2ec461954
Rajesh Naik <93809175+Rajeshsnaik@users.noreply.github.com>
2023-08-23 17:16:55 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,32 @@
+package Patterns;
+public class a1{
+    public static void main(String args[])
+    {
+        int n=4;
+        //int m=5;
+        for (int i=1;i<=n;i++)
+        {
+           for (int j=1;j<=n;j++)
+           {
+                System.out.print("* ");
+           }
+           System.out.println();
+        }
+    }
+
+    // static void swap(int[] arr,int first,int second)
+    // {
+    //     int temp=arr[first];
+    //     arr[first]=arr[second];
+    //     arr[second]=temp;
+    // }
+
+  
+}
+
+// Output : 
+
+// *****
+// *****
+// *****
+// *****
---
+++ a5.java
@@ -0,0 +1,22 @@
+package Patterns;
+public class a5{
+    public static void main(String args[])
+    {
+        int n=4;
+        int m=5;
+        for (int i=n;i>=1;i--)
+        {
+           for (int j=i;j<=m;j++)
+           
+           {
+                System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+
+}
+// **
+// ***
+// ****
+// *****
